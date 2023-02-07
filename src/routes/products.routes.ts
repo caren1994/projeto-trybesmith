a3218533cf7b7db8bba8edea215db18fc52d618b
caren <carenoliveirasilvapontes@gmail.com>
2023-02-06 20:54:38 -0500

    feat:middleares,adc middlewares nas rotas

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ProductController from '../controllers/product.controller';
+import validateName from '../middlewares/validation.name';
+import validateAmount from '../middlewares/validation.amount';
 
 const productController = new ProductController();
 const router = express.Router();
-router.post('/', productController.createP);
+router.post('/', validateName, validateAmount, productController.createP);
 router.get('/', productController.findAllP);
 export default router;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 
 import UserController from '../controllers/userController';
+import validateUsername from '../middlewares/validation.username';
+import validateVocation from '../middlewares/validation.vocation';
+import validatePassword from '../middlewares/validation.password';
+import validateLevel from '../middlewares/validation.level';
 
 const userController = new UserController();
 const router = express.Router();
-router.post('/', userController.createU);
+router.post(
+  '/', 
+  validateUsername,
+  validateVocation, 
+  validateLevel,
+  validatePassword, 
+  userController.createU,
+);
 
 export default router;
